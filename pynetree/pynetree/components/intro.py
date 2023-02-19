cf45b14cce580eada36809e2544709a22a64c8e9
blankdean <blankdean@gmail.com>
2023-02-18 21:55:07 -0500

    update pynetree

--- pynetree/pynetree/components/intro.py
+++ pynetree/pynetree/components/intro.py
@@ -2,7 +2,7 @@ import pynecone as pc
 
 def intro():
     return pc.text("""
-                Welcome to my personal website! I'm a senior software engineer at Capital One
+                I'm a senior software engineer at Capital One
                 and a part-time graduate computer science student at the Georgia Institute of Technology. I'm currently contributing to
                 research in the field of edge cloud computing.
             """, 
